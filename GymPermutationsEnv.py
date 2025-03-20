9bb4d1ed0a80409c7ee6b588d6005a87c99c1d02
Adrian Klessa <50918271+AdrianKlessa@users.noreply.github.com>
2025-03-20 16:49:42 +0100

    Add episode truncation to prevent endless episodes when agent keeps adding the same permutation

--- GymPermutationsEnv.py
+++ GymPermutationsEnv.py
@@ -8,15 +8,17 @@ from gymnasium import spaces
 class GymPermutationEnv(gym.Env):
     metadata = {"render_modes": ["console"]}
 
-    def __init__(self, alphabet_size=4, render_mode="console"):
+    def __init__(self, alphabet_size=4, render_mode="console", truncate_episodes=True):
         super(GymPermutationEnv, self).__init__()
         self.render_mode = render_mode  # Basically console-only for this use case
+        self.truncate_episodes = truncate_episodes
 
         # Set up custom variables for tracking permutations
         self.alphabet = list(range(1, alphabet_size + 1))
         env = PermutationEnvironment(self.alphabet)
-        self.permutation_env = env
-
+        self.env = env
+        self.step_count = 0
+        self.alphabet_size = alphabet_size
         # Action space
         n_actions = env.number_of_possible_permutations
         self.action_space = spaces.Discrete(n_actions)
@@ -27,8 +29,20 @@ class GymPermutationEnv(gym.Env):
         # Concatenated with same-sized one-hot encoding of which permutation was added last
         self.observation_space = spaces.MultiDiscrete([2] * (n_actions * 2))
 
+        self.bound_sizes = {
+        2: 3,
+        3: 9,
+        4: 33,
+        5: 153,
+        6: 872,
+        7: 5906,
+        8: 46205,
+        9: 408966
+    }
+
     def reset(self, seed=None, options=None):
         self.env = PermutationEnvironment(self.alphabet)
+        self.step_count = 0
         super().reset(seed=seed, options=options)
         return self.env.get_observation(), {}
 
@@ -56,7 +70,13 @@ class GymPermutationEnv(gym.Env):
 
         terminated = permutation_utils.is_superpermutation(np.array(self.env.state),
                                                            self.alphabet)  # TODO: Remove casting
+        self.step_count += 1
         truncated = False
+        # Step count is number of added permutations (minus useless actions by the agent)
+        # Bound_size[alphabet] is in characters so for an optimal superpermutation step_count will be much smaller
+        if self.truncate_episodes and self.step_count > self.bound_sizes[self.alphabet_size]:
+            truncated = True
+
         # TODO: Give reward for finishing superpermutation?
         if terminated:
             permutation_utils.check_inform_length(len(self.alphabet), self.env.state)
